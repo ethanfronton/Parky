751f657e8a9c2e2ec09195215e307750ab112b5c
ethanfronton <146748745+ethanfronton@users.noreply.github.com>
2024-12-19 11:42:42 +0100

    [ADD] --> register + new root

--- express.js
+++ express.js
@@ -5,19 +5,23 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/html/index.html", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "html/index.html"));
+  res.sendFile(path.join(__dirname, "html/index.html"));
 });
 
 app.get("/html/resa.html", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "html/resa.html"));
+  res.sendFile(path.join(__dirname, "html/resa.html"));
 });
 
 app.get("/html/about.html", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "html/about.html"));
+  res.sendFile(path.join(__dirname, "html/about.html"));
 });
 
 app.get("/html/id.html", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "html/id.html"));
+  res.sendFile(path.join(__dirname, "html/id.html"));
+});
+
+app.get("/html/register.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "html/register.html"));
 });
 
 app.listen(3001, () => {
